7b5b861074a175361137e0b6e457f96a0d1c29a1
Steven Cusey <scusey@my.gcu.edu>
2025-03-16 03:39:42 -0500

    Removed Old

--- gluten_freeway/lib/mongo_db_connection.dart
+++
@@ -1,6 +0,0 @@
-const String mongoDbConnectionString =
-    'mongodb+srv://steven:Password@cst339.awpfwjp.mongodb.net/glutenfreeway?retryWrites=true&w=majority';
-
-const String usersCollection = "users";
-const String restaurantsCollection = "restaurants";
-const String reviewsCollection = "reviews";
